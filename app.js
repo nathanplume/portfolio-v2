22ab7b45ccc62e66a1339806f50fb3c76001e6ab
Nathan Plume <nathan.plume@gmail.com>
2022-03-12 16:26:53 +0000

    styling and color added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const menu = document.querySelector('.menu-icons')
 const menuOpen = document.querySelector('.fa-bars')
 const menuClose = document.querySelector('.fa-xmark')
 const links = document.querySelector('.links')
+const nav = document.querySelector('.navbar')
 
 // ----- event listeners -----
 menu.addEventListener('click', () => {
@@ -10,3 +11,18 @@ menu.addEventListener('click', () => {
   menuClose.classList.toggle('hide-icon')
   links.classList.toggle('links-show')
 })
+
+window.onscroll = function () {
+  scrollFunction()
+}
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop >= 80 ||
+    document.documentElement.scrollTop >= 80
+  ) {
+    nav.classList.remove('transparent')
+  } else {
+    nav.classList.add('transparent')
+  }
+}
